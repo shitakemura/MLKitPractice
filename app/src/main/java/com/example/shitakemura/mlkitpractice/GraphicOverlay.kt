3c522575b760d2305d45e8d4dd3808439b6887a8
shitakemura <shitakemura@gmail.com>
2018-08-03 00:05:23 +0900

    update layout

---
+++ app/src/main/java/com/example/shitakemura/mlkitpractice/GraphicOverlay.kt
@@ -0,0 +1,8 @@
+package com.example.shitakemura.mlkitpractice
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class GraphicOverlay(context: Context, attrs: AttributeSet) : View(context, attrs) {
+}
